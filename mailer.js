c01e125800cc539e481766d0edccffff0fb1ebff
bordi <bordyanartem@gmail.com>
2018-11-13 20:53:04 +0200

    Use gsmtp

--- mailer.js
+++ mailer.js
@@ -3,39 +3,25 @@ require('dotenv').config();
 
 const nodemailer = require('nodemailer');
 
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
-nodemailer.createTestAccount((err, account) => {
-	// create reusable transporter object using the default SMTP transport
-	let transporter = nodemailer.createTransport({
-		host: 'smtp.ethereal.email',
-		port: 587,
-		secure: false, // true for 465, false for other ports
-		auth: {
-			user: account.user, // generated ethereal user
-			pass: account.pass // generated ethereal password
-		}
-	});
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL,
+		pass: process.env.PASSWORD,
+	}
+});
 
-	// setup email data with unicode symbols
-	let mailOptions = {
-		from: '"Fred Foo 👻" <foo@example.com>', // sender address
-		to: process.env.TO_EMAIL,
-		subject: 'Hello ✔', // Subject line
-		text: 'Hello world?', // plain text body
-		html: '<b>Hello world?</b>' // html body
-	};
+const mailOptions = {
+	from: 'youremail@gmail.com',
+	to: process.env.TO_EMAIL,
+	subject: 'Sending Email using Node.js',
+	text: 'That was easy!'
+};
 
-	// send mail with defined transport object
-	transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			return console.log(error);
-		}
-		console.log('Message sent: %s', info.messageId);
-		// Preview only available when sending through an Ethereal account
-		console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-		// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-		// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-	});
+transporter.sendMail(mailOptions, function(error, info){
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Email sent: ' + info.response);
+	}
 });
