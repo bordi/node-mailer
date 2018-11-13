60dfbf93de52929138767950c4e25ccc8dba77f0
bordi <bordyanartem@gmail.com>
2018-11-14 00:03:51 +0200

    Add: html templete to send

--- mailer.js
+++ mailer.js
@@ -1,27 +1,31 @@
-require('dotenv').config();
-
+require('dotenv')
+	.config();
 
 const nodemailer = require('nodemailer');
+const fs = require('fs');
 
 const transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
 		user: process.env.EMAIL,
 		pass: process.env.PASSWORD,
-	}
+	},
 });
+
+const htmlstream = fs.createReadStream('test.html');
 
 const mailOptions = {
 	from: 'youremail@gmail.com',
 	to: process.env.TO_EMAIL,
 	subject: 'Sending Email using Node.js',
-	text: 'That was easy!'
+	html: htmlstream,
 };
 
-transporter.sendMail(mailOptions, function(error, info){
-	if (error) {
+transporter.sendMail(mailOptions, function(error, info) {
+	if(error) {
 		console.log(error);
-	} else {
-		console.log('Email sent: ' + info.response);
+		return;
 	}
+
+	console.log('Email sent: ' + info.response);
 });
